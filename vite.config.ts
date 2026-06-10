36b410a51cb9067601146fe6cbd203be311c9fed
Tatiana Kedrova <Baletrot@gmail.com>
2026-06-10 12:09:00 +0300

    added vitest/ui package; added types key to tsconfig.json to use global test variables; added test key to vite.config with parameters; added setupTest file;

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
 
@@ -9,4 +9,9 @@ export default defineConfig({
     open: true, // Automatically opens the browser
   },
   base: "/HW-project-ITIncubator/",
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
 });
