20df6a4cbb345bfea40c189b8877cc2d89005762
Tatiana Kedrova <Baletrot@gmail.com>
2026-06-17 14:23:04 +0300

    added chatReducer to RTK store;

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
+import { chatReducer } from "@/features/chatSlice";
 import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    chatReducer: chatReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- src/features/chatSlice.ts
+++ src/features/chatSlice.ts
@@ -1,9 +1,15 @@
 import { MessageType } from "@/types/chat.project.types";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 export type ChatState = MessageType[];
-// export const chatSlice = createSlice({
-//     name: "chat-app",
-//     initialState: [],
-    
+export const chatSlice = createSlice({
+  name: "chat_app",
+  initialState: [] as MessageType[],
+  reducers: {
+    addMessage: (state: MessageType[], action: PayloadAction<MessageType>) => {
+      state.push(action.payload);
+    },
+  },
+});
 
-// })
+export const { addMessage } = chatSlice.actions;
+export const chatReducer = chatSlice.reducer;
